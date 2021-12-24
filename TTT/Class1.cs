e6540022eb63abdb035c405a300aec7579c64dd6
USER <USER@DESKTOP-BL7QGDJ>
2021-12-24 22:41:15 +0200

    Add project files.

---
+++ TTT/Class1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTT
+{
+    public class Enemy
+    {
+        public char[] grid = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        public List<int> winningComb = new List<int>() { 123, 456, 789, 147, 258, 369, 159, 357 };
+        public List<int> winningComb3 = new List<int>() { 123, 456, 789, 147, 258, 369, 159, 357 };
+        public List<int> winningComb2 = new List<int>() { 123, 456, 789, 147, 258, 369, 159, 357 };
+        public List<int> X = new List<int>() { };
+        public List<int> OM = new List<int>() { };
+        public List<int> XO = new List<int>() { }; // the positions where X and O stored
+        public List<int> O = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // the positions where O stored
+        public List<int> Identify = new List<int>() { 123, 456, 789, 147, 258, 369, 159, 357 };
+        public List<int> Win = new List<int>() { 123, 456, 789, 147, 258, 369, 159, 357 };
+        public List<int> possible = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        public List<int> possible2 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        public int currComb = 0;
+        string winComb = "";
+        public Enemy()
+        {
+            RemoveComb();
+        }
+
+        public void IdentifyComb()
+        {
+            for (int i = 0; i < winningComb.Count; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < X.Count; j++)
+                {
+                    if (winningComb[i].ToString().Contains(X[j].ToString()))
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Identify[0] = winningComb[i];
+                    winningComb.Remove(winningComb[i]);
+                }
+            }
+        }
+
+        public void RemoveComb() //Removing combinations for O
+        {
+            for (int i = 0; i < winningComb2.Count; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < possible2.Count; j++)
+                {
+                    if (winningComb2[i].ToString().Contains(possible2[j].ToString()))
+                    {
+                        count++;
+                    }
+                }
+                if (count != 3)
+                {
+                    winningComb2.Remove(winningComb2[i]);
+                }
+            }
+            
+        }
+
+        public void MoveWin()
+        {
+            try
+            {
+                char[] mov = winningComb2[0].ToString().ToArray();
+
+                int mov2 = 0;
+                int count = 0;
+
+                for (int i = 0; i < mov.Length; i++)
+                {
+                    for (int j = 0; j < O.Count; j++)
+                    {
+                        if (mov[i] == O[j] + 48)
+                        {
+                            mov2 = mov[i] - 48;
+                            break;
+                        }
+                        else
+                        {
+                            count++;
+                        }
+                    }
+                    if (count == 0)
+                    {
+                        break;
+                    }
+                }
+
+
+                grid[mov2 - 1] = 'O';
+                XO.Add(mov2);
+                X.Remove(mov2);
+                possible.Remove(mov2);
+                O.Remove(mov2);
+                OM.Add(mov2);
+            }
+            catch
+            {
+                MoveBlock();
+            }
+
+        }
+
+
+        public void MoveBlock()
+        {
+            try
+            {
+                char[] mov = Identify[0].ToString().ToArray();
+                int mov2 = 0;
+                int count = 0;
+
+                for (int i = 0; i < mov.Length; i++)
+                {
+                    for (int j = 0; j < possible.Count; j++)
+                    {
+                        if (mov[i] == possible[j] + 48)
+                        {
+                            mov2 = mov[i] - 48;
+                            break;
+                        }
+                        else
+                        {
+                            count++;
+                        }
+                    }
+                    if (count == 0)
+                    {
+                        break;
+                    }
+                }
+
+
+                if (Identify[0] != 0)
+                {
+                    grid[mov2 - 1] = 'O';
+                    XO.Add(mov2);
+                    O.Remove(mov2);
+                    X.Remove(mov2);
+                    OM.Add(mov2);
+                    possible.Remove(mov2);
+                }
+            }
+            catch
+            {
+                MoveWin();
+            }
+        }
+
+
+        public bool InArr(int value)
+        {
+            bool inArr = false;
+            for (int i = 0; i < XO.Count; i++)
+            {
+                if (XO[i] == value)
+                {
+                    inArr = true;
+                }
+            }
+            return inArr;
+        }
+
+        public bool InPossible(string str)
+        {
+            bool inPoss = false;
+            for (int j = 0; j < str.Length; j++)
+            {
+                int count = 0;
+                for (int i = 0; i < possible.Count; i++)
+                {
+                    if (possible[i].ToString().Equals(str.ToString()))
+                    {
+                        count++;
+                    }
+                }
+                if (count > 0)
+                {
+                    inPoss = true;
+                    break;
+                }
+            }
+            return inPoss;
+        }
+
+        public bool isWin()
+        {
+            bool isWin = false;
+            for (int i = 0; i < winningComb3.Count; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < OM.Count; j++)
+                {
+                    if (winningComb3[i].ToString().Contains(OM[j].ToString()))
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    isWin = true;
+                    break;
+                }
+            }
+            return isWin;
+        }
+
+
+        public bool isIdentify()
+        {
+            bool isIdentify = false;
+            for (int i = 0; i < winningComb3.Count; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < X.Count; j++)
+                {
+                    if (winningComb3[i].ToString().Contains(X[j].ToString()) || InPossible(winningComb3[i].ToString()))
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    isIdentify = true;
+                    break;
+                }
+            }
+            return isIdentify;
+        }
+
+
+    }
+}
---
+++ TTT/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TTT // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+
+        public static bool isOver(Enemy ene)
+        {
+            bool isOver = false;
+            for (int i = 0; i < ene.winningComb3.Count; i++)
+            {
+                int countX = 0;
+                int countO = 0;
+                for (int j = 0; j < ene.X.Count; j++)
+                {
+                    if (ene.winningComb3[i].ToString().Contains(ene.X[j].ToString()))
+                    {
+                        countX++;
+                    }
+                }
+
+                for (int x = 0; x < ene.OM.Count; x++)
+                {
+                    if (ene.winningComb3[i].ToString().Contains(ene.OM[x].ToString()))
+                    {
+                        countO++;
+                    }
+                }
+                if (countX == 3 || countO == 3)
+                {
+                    isOver = true;
+                }
+            }
+            return isOver;
+        }
+
+        public static void Board(Enemy ene)
+        {
+            Console.WriteLine($"|  { ene.grid[0]}  | { ene.grid[1]}   | { ene.grid[2]}");
+            Console.WriteLine($" ----  ---- ----");
+            Console.WriteLine($"|  {ene.grid[3]}  | {ene.grid[4]}   | {ene.grid[5]}");
+            Console.WriteLine($" ----  ---- ----");
+            Console.WriteLine($"|  {ene.grid[6]}  | {ene.grid[7]}   | {ene.grid[8]}");
+            Console.WriteLine(" ----  ---- ----");
+        }
+
+
+        public static void Main(string[] args)
+        {
+
+            Enemy ene = new Enemy();
+            int attempts = 0;
+            int count = 0;
+
+
+            /////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////
+
+            Board(ene);
+            Console.WriteLine("Enter position: ");
+            int position = int.Parse(Console.ReadLine());
+            bool Over = isOver(ene);
+            while (position != 10 && !Over)
+            {
+                if (!Over)
+                {
+
+
+                    ene.grid[position - 1] = 'X';
+                    ene.X.Add(position);
+                    ene.XO.Add(position);
+                    ene.possible.Remove(position);
+                    ene.possible2.Remove(position);
+                    Over = isOver(ene);
+
+                    Board(ene);
+                    ene.RemoveComb();
+                    Console.WriteLine("Winning comb for O");
+                    for (int i = 0; i < ene.winningComb2.Count; i++)
+                    {
+                        Console.Write(ene.winningComb2[i] + " ");
+                    }
+                    //Console.WriteLine(ene.winningComb2[0]);
+                    Console.WriteLine("Possible");
+                    for (int i = 0; i < ene.possible.Count; i++)
+                    {
+                        Console.Write(ene.possible[i] + " ");
+                    }
+                    Console.WriteLine("Identify");
+
+                    ene.IdentifyComb();
+                    Console.WriteLine(ene.Identify[0]);
+
+                    Console.WriteLine(attempts);
+                    Console.WriteLine();
+                    attempts++;
+                    Console.WriteLine(attempts);
+                    if (!isOver(ene))
+                    {
+
+
+                        if (ene.isWin())
+                        {
+                            ene.RemoveComb();
+                            ene.isWin();
+                            ene.MoveWin();
+                        }
+                        else if (ene.isIdentify())
+                        {
+                            ene.IdentifyComb();
+                            ene.MoveBlock();
+                        }
+                        else
+                        {
+                            ene.RemoveComb();
+                            ene.isWin();
+                            ene.MoveWin();
+                        }
+
+
+                    }
+                    // Print the game board
+                    /*if (attempts % 2 == 1)
+                    {
+                        ene.RemoveComb();
+                        position = ene.MoveWin();
+                        Console.WriteLine(ene.winningComb[0]);
+                        ene.grid[position - 1] = 'O';
+                        ene.XO.Add(position);
+                        ene.possible.Remove(position);
+                    }
+                    else
+                    {
+                        int currComb = ene.IdentifyComb();
+                        position = ene.MoveBlock(currComb);
+                        Console.WriteLine(ene.currComb);
+                        ene.grid[position - 1] = 'O';
+                        ene.XO.Add(position);
+                        ene.possible.Remove(position);
+                    }
+                    */
+                    Board(ene);
+                    if (!isOver(ene))
+                    {
+                        Console.WriteLine("Enter position: ");
+                        position = int.Parse(Console.ReadLine());
+                        Console.Clear();
+                    }
+                }
+            }
+        }
+    }
+}
